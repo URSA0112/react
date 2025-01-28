4a42f6b37acd28927c3e02fb50e7d5f1bf599098
URSA0112 <Nimone99@gmail.com>
2025-01-28 12:40:39 +0800

    cards on react

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./globals.css";
 
-export default function Home() {
+const people = [
+  {
+    id: 1,
+    name: "Alice",
+    age: 25,
+    email: "alice@example.com",
+    city: "New York"
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 30,
+    email: "bob@example.com",
+    city: "Los Angeles"
+  },
+]
+
+
+function Card(props) {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="cards-container">
+        {/* card1 */}
+        <div className="cards" id="card1">
+          <div className="left-side">
+            <div className="left-up">
+              <div className="left-up-text">{people[0].name}</div>
+            </div>
+            <div className="left-bottom">
+              <p className="left-bottom-info">000-123-456-7890</p>
+              <p className="left-bottom-info">email@yourdomain.com</p>
+              <p className="left-bottom-info">
+                Your address goes here 125 Street, USA
+              </p>
+            </div>
+          </div>
+          <div className="right-side">
+            <div className="right-up"></div>
+            <div className="rigth-bottom"></div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* end of card-container */}
+      </div>
     </div>
   );
+}
+
+export default function Home() {
+
+
+  const cards = [];
+  for (let i = 0; i <3; i++) {
+    cards.push(<Card></Card>);
+  
+  }
+  return <div>{cards}</div>;
 }
